052c31db220f6455def0f5aa93cd953817ed0096
Ivan Nemshilov <ivannemshilov@Ivans-Mac.local>
2018-11-05 04:17:26 -0800

    add base storyboard

---
+++ Lingvinity/Lingvinity/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Lingvinity
+//
+//  Created by Ivan Nemshilov on 24/10/2018.
+//  Copyright © 2018 IT-Berries. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Lingvinity/Lingvinity/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lingvinity
+//
+//  Created by Ivan Nemshilov on 24/10/2018.
+//  Copyright © 2018 IT-Berries. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
