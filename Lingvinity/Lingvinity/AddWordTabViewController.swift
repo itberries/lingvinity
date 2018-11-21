117d016c48a969b3adb107963cfc273e89c37ff2
Ivan Nemshilov <ivannemshilov@Ivans-Mac.local>
2018-11-21 04:03:35 -0800

    add work with camera

--- Lingvinity/Lingvinity/AddWordTabViewController.swift
+++ Lingvinity/Lingvinity/AddWordTabViewController.swift
@@ -8,12 +8,39 @@
 
 import UIKit
 
-class AddWordTabViewController: UIViewController {
-
+class AddWordTabViewController:
+    UIViewController,
+    UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate {
+    
+    @IBOutlet weak var Camera: UIButton!
+    @IBOutlet weak var PhotoLibrary: UIButton!
+    @IBOutlet weak var ImageDisplay: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func PhotoLibraryAction(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func CameraAction(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        ImageDisplay.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
-
