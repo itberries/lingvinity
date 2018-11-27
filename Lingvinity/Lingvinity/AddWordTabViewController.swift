642ce8f8435d7ca317fb8078b55ce34e6e288f89
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-11-27 22:51:18 +0300

    Inceptiov3 model added, Word prediction by photo, Refactoring

--- Lingvinity/Lingvinity/AddWordTabViewController.swift
+++ Lingvinity/Lingvinity/AddWordTabViewController.swift
@@ -7,40 +7,109 @@
 //
 
 import UIKit
+import CoreML
 
-class AddWordTabViewController:
+class AddWordTabViewController :
     UIViewController,
-    UIImagePickerControllerDelegate,
     UINavigationControllerDelegate {
     
-    @IBOutlet weak var Camera: UIButton!
-    @IBOutlet weak var PhotoLibrary: UIButton!
-    @IBOutlet weak var ImageDisplay: UIImageView!
+    @IBOutlet weak var camera: UIButton!
+    @IBOutlet weak var photoLibrary: UIButton!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var recognitionResult: UILabel!
+    
+    var model: Inceptionv3!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func PhotoLibraryAction(_ sender: UIButton) {
+    override func viewWillAppear(_ animated: Bool) {
+        model = Inceptionv3()
+    }
+    
+    @IBAction func openPhotoLibrary(_ sender: UIButton) {
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .photoLibrary
+        picker.allowsEditing = false
         
         present(picker, animated: true, completion: nil)
     }
     
-    @IBAction func CameraAction(_ sender: UIButton) {
+    @IBAction func openCamera(_ sender: UIButton) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return
+        }
+        
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .camera
+        picker.allowsEditing = false
         
         present(picker, animated: true, completion: nil)
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        ImageDisplay.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+}
+
+extension AddWordTabViewController : UIImagePickerControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        recognitionResult.text = "Analyzing Image..."
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        
+        let convertionResult = convertImageFormat(for: image)
+        if convertionResult == nil {
+            return
+        }
+        
+        photoImageView.image = convertionResult?.newImage
+        
+        guard let prediction = try? model.prediction(image: (convertionResult?.pixelBuffer)!) else {
+            return
+        }
+        
+        recognitionResult.text = "\(prediction.classLabel)."
+    }
+    
+    func convertImageFormat(for image: UIImage) -> (newImage: UIImage, pixelBuffer: CVPixelBuffer)? {
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        photoImageView.image = newImage
+        
+        return (newImage, pixelBuffer!)
     }
     
 }
