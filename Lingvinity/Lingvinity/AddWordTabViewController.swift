7e8545d184c08a83b91a817d95033097b899bc65
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-12-11 14:46:07 +0300

    Prediction service with top 5 results

--- Lingvinity/Lingvinity/AddWordTabViewController.swift
+++ Lingvinity/Lingvinity/AddWordTabViewController.swift
@@ -18,8 +18,8 @@ class AddWordTabViewController :
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var recognitionResult: UILabel!
     
-    var model: MobileNet!
     let imageStorage = ImageStorageService()
+    let predictionService = PredictionService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +27,7 @@ class AddWordTabViewController :
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        model = MobileNet()
+        predictionService.loadModel()
     }
     
     @IBAction func openPhotoLibrary(_ sender: UIButton) {
@@ -67,55 +67,14 @@ extension AddWordTabViewController : UIImagePickerControllerDelegate {
             return
         }
         
-        let convertionResult = convertImageFormat(for: image)
-        if convertionResult == nil {
-            return
+        let predictionResult = predictionService.getTopPredictionResults(for: image)
+        if let convertedImage = predictionResult?.convertedImage,
+            let prediction = predictionResult?.predictions.first {
+            photoImageView.image = convertedImage
+            // TODO: добавить выбор одного слова для сохранения в словарь из топ 5
+            recognitionResult.text = prediction.0
+            imageStorage.save(image: convertedImage, withName: "1")
         }
-        photoImageView.image = convertionResult?.newImage
-        
-        guard let prediction = try? model.prediction(image: (convertionResult?.pixelBuffer)!) else {
-            return
-        }
-        recognitionResult.text = "\(prediction.classLabel)."
-        
-        imageStorage.save(image: convertionResult!.newImage, withName: "1")
-    }
-    
-    func convertImageFormat(for image: UIImage) -> (newImage: UIImage, pixelBuffer: CVPixelBuffer)? {
-        
-        // TODO: fix image size on storybord to 224x224
-        
-        let widthToConvert = 224
-        let heightToConvert = 224
-        
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: widthToConvert, height: heightToConvert), true, 2.0)
-        image.draw(in: CGRect(x: 0, y: 0, width: widthToConvert, height: heightToConvert))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-        var pixelBuffer : CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-        guard (status == kCVReturnSuccess) else {
-            return nil
-        }
-        
-        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-        
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
-        
-        context?.translateBy(x: 0, y: newImage.size.height)
-        context?.scaleBy(x: 1.0, y: -1.0)
-        
-        UIGraphicsPushContext(context!)
-        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
-        UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        photoImageView.image = newImage
-        
-        return (newImage, pixelBuffer!)
     }
     
 }
