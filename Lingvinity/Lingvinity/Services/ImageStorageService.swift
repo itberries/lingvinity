7bc81ba4f23a9e69025903494a99c58d53c75d7e
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-12-11 09:54:40 +0300

    add image storage service file

---
+++ Lingvinity/Lingvinity/Services/ImageStorageService.swift
@@ -0,0 +1,9 @@
+//
+//  ImageStorageService.swift
+//  Lingvinity
+//
+//  Created by Anastasia Puchnina on 11/12/2018.
+//  Copyright © 2018 IT-Berries. All rights reserved.
+//
+
+import Foundation
