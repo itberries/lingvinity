ef99c194ec47d8d84cef3c54252018c3097dcd3e
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-12-12 18:21:54 +0300

    words in db, refactoring

--- Lingvinity/Lingvinity/AddWordTabViewController.swift
+++ Lingvinity/Lingvinity/AddWordTabViewController.swift
@@ -157,39 +157,6 @@ extension AddWordTabViewController : UIImagePickerControllerDelegate {
         setResultWord(index: selectedWordIndex - 1)
     }
     
-    func convertImageFormat(for image: UIImage) -> (newImage: UIImage, pixelBuffer: CVPixelBuffer)? {
-        
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
-        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        
-        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
-        var pixelBuffer : CVPixelBuffer?
-        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-        guard (status == kCVReturnSuccess) else {
-            return nil
-        }
-        
-        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
-        
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
-        
-        context?.translateBy(x: 0, y: newImage.size.height)
-        context?.scaleBy(x: 1.0, y: -1.0)
-        
-        UIGraphicsPushContext(context!)
-        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
-        UIGraphicsPopContext()
-        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-        photoImageView.image = newImage
-        
-        return (newImage, pixelBuffer!)
-	
-    }
-    
     func translateWords(wordsArray: [String], finished: @escaping (([String]) -> Void)) {
         let url = URL(string: "https://translate.yandex.net/api/v1.5/tr.json/translate?lang=ru&key=trnsl.1.1.20181205T100748Z.63ceec24b0413b7b.3da3dd7fc83a0cdcbb8bd4ae6a4d733316e054a9&")!
         var request = URLRequest(url: url)
