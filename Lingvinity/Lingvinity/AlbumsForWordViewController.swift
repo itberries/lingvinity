2fdec9e3a28e94584c4304d7ecde316a8223d163
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-12-19 16:04:51 +0300

    Add AlbumsForWord View and ViewController

---
+++ Lingvinity/Lingvinity/AlbumsForWordViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AlbumsForWordViewController.swift
+//  Lingvinity
+//
+//  Created by Anastasia Puchnina on 19/12/2018.
+//  Copyright © 2018 IT-Berries. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AlbumsForWordViewController: UIViewController {
+    
+    var word : WordModel?
+    var albums = [AlbumModel]()
+    var selectedAlbums = [AlbumModel]()
+    
+    let databaseService : StorageService?
+    
+    let wordCellIdentifier = "AlbumWordTableViewCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Select albums.."
+        
+        //albums = databaseService.findAllWordsByAlbumId(groupId: (self.album?.id)!)
+        
+        //wordsTableView.dataSource = self
+        //wordsTableView.delegate = self
+        
+        //wordsTableView.register(UINib.init(nibName: "AlbumWordTableViewCell", bundle: nil), forCellReuseIdentifier: wordCellIdentifier)
+        
+    }
+
+}
